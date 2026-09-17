5db09525659811ab818d5bf3b37db3dc1c73b5be
ltiffany26007 <tiffanyli034@gmail.com>
2026-09-17 14:31:30 -0400

    added classes

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,5 +1,5 @@
 package calendar;
 
 public class Calendar {
-
+    
 }
---
+++ src/calendar/Day.java
@@ -0,0 +1,19 @@
+package calendar;
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author Tiffany
+ *  @version Sep 17, 2026
+ */
+public class Day
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
