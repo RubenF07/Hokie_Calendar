91d63e8b422d2a80b9fcd352f0dcba030c4f75d8
ssarah08 <sarahgschallon@gmail.com>
2026-09-17 15:46:01 -0400

    Day and Month class updates

--- src/calendar/Month.java
+++ src/calendar/Month.java
@@ -1,29 +1,9 @@
 package calendar;
-<<<<<<< Updated upstream
-// -------------------------------------------------------------------------
-/**
- *  Write a one-sentence summary of your class here.
- *  Follow it with additional details about its purpose, what abstraction
- *  it represents, and how to use it.
- * 
- *  @author Tiffany
- *  @version Sep 17, 2026
- */
-public class Month
-{
-    //~ Fields ................................................................
 
-    //~ Constructors ..........................................................
-
-    //~Public  Methods ........................................................
-
-=======
-
-// -------------------------------------------------------------------------
 /**
  * Creates a day object that contains and manages events for a specific day
  * 
- * @author sarah
+ * @author Sarah
  * @version Sep 17, 2026
  */
 public class Month
@@ -33,10 +13,11 @@ public class Month
     private int numDays;
     private String startWeekDay;
     private Day[] entireMonth;
-    
+
     // ----------------------------------------------------------
     /**
      * Create a new Month object.
+     * 
      * @param monthName
      * @param numDays
      * @param startWeekDay
@@ -47,61 +28,69 @@ public class Month
         this.monthName = monthName;
         this.numDays = numDays;
         this.startWeekDay = startWeekDay;
-        
-        entireMonth = new Day[numDays];   
+
+        entireMonth = new Day[numDays];
     }
-    
-    
+
+
     // ----------------------------------------------------------
     // ~Public Methods ........................................................
     /**
      * get the name of the month
+     * 
      * @return monthName
-     */ 
+     */
     public String getMonth()
     {
         return monthName;
     }
-    
+
+
     // ----------------------------------------------------------
     /**
      * get the number of days in the month
+     * 
      * @return numDays
      */
     public int getNumDays()
     {
         return numDays;
     }
-    
+
+
     // ----------------------------------------------------------
     /**
      * get the day of the week the first day of the month occurs
+     * 
      * @return startWeekDay
      */
     public String getStartWeekDay()
     {
         return startWeekDay;
     }
-    
+
+
     // ----------------------------------------------------------
     /**
      * Place a description of your method here.
-     * @param dayNumber: nth of the month
+     * 
+     * @param dayNumber:
+     *            nth of the month
      * @return specific Day from month array
      */
     public Day getDay(int dayNumber)
     {
-        return entireMonth[dayNumber-1];
+        return entireMonth[dayNumber - 1];
     }
-    
+
+
     public String toString()
     {
         String monthArrayString = "";
-        for(int i = 0; i < numDays; i++)
+        for (int i = 0; i < numDays; i++)
         {
-            monthArrayString += (entireMonth[i]).getDate() + " "; 
+            monthArrayString += (entireMonth[i]).getDate() + " ";
         }
         return monthArrayString;
     }
->>>>>>> Stashed changes
 }
