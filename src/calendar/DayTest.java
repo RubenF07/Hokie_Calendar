4a733e10e0a552d80b180276b8e73a356a355f73
ssarah08 <sarahgschallon@gmail.com>
2026-09-23 15:50:09 -0400

    Updated DayTest with event description

--- src/calendar/DayTest.java
+++ src/calendar/DayTest.java
@@ -14,15 +14,17 @@ public class DayTest
     // ~ Set Up ..........................................................
     public void setUp()
     {
-        allDay = new Event("All Day", "Here", true);
+        allDay = new Event("All Day", "This is all day", "Here", true);
         timedFirst = new TimedEvent(
             "Yoga",
+            "Weekly Class",
             "Gym",
             true,
             new Time(11, 30, true),
             new Time(1, 30, false));
         timedSecond = new TimedEvent(
             "Birthday",
+            "Mom's turning 40",
             "Home",
             true,
             new Time(5, 30, false),
@@ -32,24 +34,30 @@ public class DayTest
 
 
     // ~Test Methods ........................................................
+    // ----------------------------------------------------------
+    /**
+     * test addEvent()
+     */
     public void testAddEvent()
     {
         assertEquals("Date: 22\n", today.toString());
 
         today.addEvent(timedSecond);
         assertEquals(1, today.getListOfEvents().size());
-        assertEquals("Date: 22\nBirthday at Home from 5:30-7:45\n", today.toString());
+        assertEquals(
+            "Date: 22\nBirthday\nMom's turning 40\nat Home\n5:30 PM-7:45 PM\n",
+            today.toString());
 
         today.addEvent(allDay);
         assertEquals(2, today.getListOfEvents().size());
         assertEquals(
-            "Date: 22\nBirthday at Home from 5:30-7:45\nAll Day at Here\n",
+            "Date: 22\nBirthday\nMom's turning 40\nat Home\n5:30 PM-7:45 PM\nAll Day\nThis is all day\nat Here\n",
             today.toString());
 
         today.addEvent(timedFirst);
         assertEquals(3, today.getListOfEvents().size());
         assertEquals(
-            "Date: 22\nYoga at Gym from 11:30-1:30\nBirthday at Home from 5:30-7:45\nAll Day at Here\n",
+            "Date: 22\nYoga\nWeekly Class\nat Gym\n11:30 AM-1:30 PM\nBirthday\nMom's turning 40\nat Home\n5:30 PM-7:45 PM\nAll Day\nThis is all day\nat Here\n",
             today.toString());
     }
 }
